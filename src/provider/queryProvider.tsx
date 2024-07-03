e6def2de2488232939b2c8b7f7cf17a698722ebb
seungju24 <lsj001018@nanugo.kr>
2024-07-03 20:39:49 +0900

    init : 초기세팅

---
+++ src/provider/queryProvider.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+interface QueryProviderProps {
+    children: React.ReactNode;
+}
+
+export default function QueryProvider({ children }: QueryProviderProps) {
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                refetchOnWindowFocus: true,
+                retryOnMount: true,
+                refetchOnReconnect: false,
+                retry: false,
+            },
+        },
+    });
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    );
+}
