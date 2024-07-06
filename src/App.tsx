95670b4741d1d0764abb37f4375df1fb3f214391
chae-young <cycy8527@gmail.com>
2024-07-06 16:37:14 +0900

    Add: shadcn 모듈

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { Button } from './components/ui/button';
+
 export default function App() {
     return (
         <div className="flex items-center justify-center">
+            <div>
+                <Button>Click me</Button>
+            </div>
             <h2 className="text-xl font-bold text-blue-500">Tailwind Test</h2>
         </div>
     );
