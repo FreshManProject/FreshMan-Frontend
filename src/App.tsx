efb5a181a229dd3dac1f6c4bf65c3a47573725e4
chae-young <cycy8527@gmail.com>
2024-07-07 16:43:26 +0900

    #1 Style: 공통 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import TopHeader from './components/common/TopHeader/TobHeader';
 import { Button } from './components/ui/button';
 
 export default function App() {
     return (
         <div className="flex items-center justify-center">
+            <TopHeader>테스트</TopHeader>
             <div>
                 <Button>Click me</Button>
             </div>
