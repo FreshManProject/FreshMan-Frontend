c1f8d017b7e56a0cc3dc8c72118ec9528923ec8d
이챙 <cycy8527@gmail.com>
2024-07-22 09:57:28 +0900

    Style: eslint error (#19)

--- src/components/common/Button/GrayBorderButton.tsx
+++ src/components/common/Button/GrayBorderButton.tsx
@@ -7,7 +7,7 @@ export default function GrayBorderButton({ children }: IGrayBorderButtonProps) {
         <button
             type={'button'}
             className={
-                'border-gray200 flex h-[3.75rem] items-center justify-center rounded-lg border px-3 py-1 text-title3_b text-bk'
+                'flex h-[3.75rem] items-center justify-center rounded-lg border border-gray200 px-3 py-1 text-title3_b text-bk'
             }
         >
             {children}
