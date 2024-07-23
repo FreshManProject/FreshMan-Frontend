8d0178b2b01972858c3ed5989f27744e850a9caf
이챙 <cycy8527@gmail.com>
2024-07-23 18:23:19 +0900

    Fix: msw 수정 (#22)

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,27 +1,38 @@
 import { rest } from 'msw';
 
 const handlers = [
-    // http.get('/api/users', () => {
-    //     return HttpResponse.json({
-    //         name: '김남성',
-    //         email: 'kimssd@naver.com',
-    //         address: '서울특별시 강남구 삼성동 22-3',
-    //         addressDetail: '스타벅스 문앞',
-    //         review: 20,
-    //         heart: 200,
-    //     });
-    // }),
-    // http.get('/api/search/recent', () => {
-    //     return HttpResponse.json({
-    //         data: ['남성 왁스', '파운데이션', '향수', '촉촉한 립', '핸드 크림'],
-    //     });
-    // }),
-    // http.get('/api/search/option', () => {
-    //     return HttpResponse.json({
-    //         option: ['전체', '베이스', '향수', '립스틱'],
-    //     });
-    // }),
-
+    rest.get('/api/users', (req, res, ctx) => {
+        return res(
+            ctx.json({
+                name: '김남성',
+                email: 'kimssd@naver.com',
+                address: '서울특별시 강남구 삼성동 22-3',
+                addressDetail: '스타벅스 문앞',
+                review: 20,
+                heart: 200,
+            }),
+        );
+    }),
+    rest.get('/api/search/recent', (req, res, ctx) => {
+        return res(
+            ctx.json({
+                data: [
+                    '남성 왁스',
+                    '파운데이션',
+                    '향수',
+                    '촉촉한 립',
+                    '핸드 크림',
+                ],
+            }),
+        );
+    }),
+    rest.get('/api/search/option', (req, res, ctx) => {
+        return res(
+            ctx.json({
+                option: ['전체', '베이스', '향수', '립스틱'],
+            }),
+        );
+    }),
     rest.get('/products', (req, res, ctx) => {
         // const url = new URL(request.url);
         // const categorySeq = url.searchParams.get('categorySeq');
