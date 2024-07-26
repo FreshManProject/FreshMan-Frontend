78eba140082e79faf048af74d0e2469efc4e5d9f
CsLsj <lsj001018@naver.com>
2024-07-26 14:07:53 +0900

    MSW : msw 수정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -60,6 +60,48 @@ const handlers = [
             ],
         });
     }),
+    http.get('/api/contact', () => {
+        return HttpResponse.json({
+            data: [
+                {
+                    id: 1,
+                    image: 'https://sitem.ssgcdn.com/68/30/98/spclprc/1000282983068_sp.jpg',
+                    date: '2024.07.23',
+                    author: 'test',
+                    title: '피부관련해서 질문있습니다.',
+                    description:
+                        '안녕하세요 피부관련해서 질문드릴 점이 있습니다.',
+                    product:
+                        '[THREE TO EIGHTY] Essential Color Socks (15colors)',
+                    answer: '안녕하세요 고객님',
+                },
+                {
+                    id: 2,
+                    image: 'https://sitem.ssgcdn.com/68/30/98/spclprc/1000282983068_sp.jpg',
+                    date: '2024.07.23',
+                    author: 'test',
+                    title: '피부관련해서 질문있습니다.',
+                    description:
+                        '안녕하세요 피부관련해서 질문드릴 점이 있습니다.',
+                    product:
+                        '[THREE TO EIGHTY] Essential Color Socks (15colors)',
+                    answer: '',
+                },
+                {
+                    id: 3,
+                    image: 'https://sitem.ssgcdn.com/68/30/98/spclprc/1000282983068_sp.jpg',
+                    date: '2024.07.23',
+                    author: 'test',
+                    title: '피부관련해서 질문있습니다.',
+                    description:
+                        '안녕하세요 피부관련해서 질문드릴 점이 있습니다.',
+                    product:
+                        '[THREE TO EIGHTY] Essential Color Socks (15colors)',
+                    answer: '안녕하세요 고객님',
+                },
+            ],
+        });
+    }),
 ];
 
 export default handlers;
