7de4acf5d5cb4d1e817c6ddee10010b172990c98
CsLsj <lsj001018@naver.com>
2024-07-30 22:47:39 +0900

    fix : 소셜 로그인 리다이렉트 페이지 수정 및 accessToken 쿠키에 저장하도록 수정

--- src/pages/Login/NaverSocialLoginPage.tsx
+++
@@ -1,5 +0,0 @@
-export default function NaverSocialLoginPage() {
-    const code = new URL(document.location.toString()).searchParams.get('code');
-    console.log(code);
-    return <div>123</div>;
-}
