2c9101b3d0c7ac8f2ffbd2bfdd42343a17f8728e
yoonlee201 <93956761+yoonlee201@users.noreply.github.com>
2024-08-13 00:19:40 -0400

    [Add] cart 더미 데이터 추가 및 GET api 구축 (#45)

--- src/apis/user/index.ts
+++ src/apis/user/index.ts
@@ -14,6 +14,32 @@ export default async function getLikeList(): Promise<productListType> {
         throw Error;
     }
 }
+export async function getCartList(): Promise<productListType> {
+    try {
+        const response = await axios.get('/carts');
+        if (response.data) return response.data;
+        throw new Error(
+            `Unexpected response : ${response.status} ${response.statusText}`,
+        );
+    } catch (error) {
+        console.error(error);
+        throw Error;
+    }
+}
+export async function getInquiryList() {
+    try {
+        const response = await axios.get('/api/contact');
+        if (response.data) {
+            return response.data.data;
+        }
+        throw new Error(
+            `Unexpected response : ${response.status} ${response.statusText}`,
+        );
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
 
 export async function getInquiryList() {
     try {
