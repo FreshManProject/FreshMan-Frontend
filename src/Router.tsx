1e60dd4e9f94e85dba0dd22c47f526fba0436aed
yoonlee201 <93956761+yoonlee201@users.noreply.github.com>
2024-08-13 00:24:02 -0400

    [Feat] cart 라우터 추가 및 카트 페이지 생성 (#45)

--- src/Router.tsx
+++ src/Router.tsx
@@ -39,7 +39,7 @@ export default function Router() {
             children: [
                 {
                     path: '',
-                    element: <div>Cart</div>,
+                    element: <CartPage />,
                 },
             ],
         },
