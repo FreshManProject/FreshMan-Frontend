6fb5c0eb05d8d88b37918d0cc273d15a6643c26c
yoonlee201 <93956761+yoonlee201@users.noreply.github.com>
2024-08-20 07:31:15 -0400

    [Feat] add Infinite Scroll

--- src/apis/products/index.ts
+++ src/apis/products/index.ts
@@ -7,6 +7,8 @@ import axios from 'axios';
 import { ProductDetailType } from '@/types/Product/productDetail';
 import { BASE_URL, axiosAuth } from '..';
 
+export const rowsPerPage = 10;
+
 export async function getProductList(
     params: productListParamsType,
 ): Promise<productListType> {
