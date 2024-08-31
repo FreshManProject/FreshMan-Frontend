00e3cb8b538baff3ee4a59c4f2da2308487f4171
chae-young <cycy8527@gmail.com>
2024-08-31 22:08:55 +0900

    Feat: 로그인시 토큰 발급 및 재발급으로 로그인 유지 (#36)

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,5 +1,5 @@
-import axios from 'axios';
-// TODO: login 로직 보류
+import axios, { AxiosRequestConfig } from 'axios';
+
 export const BASE_URL = process.env.REACT_APP_FRESHMAN_PUBLIC_API_URL;
 // export const BASE_URL = 'http://localhost:3000';
 
@@ -19,29 +19,46 @@ export const axiosAuth = axios.create({
     timeout: 10 * 1000,
 });
 
+// 엑세스토큰 재발급
 const refreshAccessToken = async () => {
     try {
         const response = await axios.post(
             `${process.env.REACT_APP_FRESHMAN_PUBLIC_API_URL}reissue`,
+            {},
             {
                 headers: {
                     'Content-Type': 'application/json',
                 },
                 withCredentials: true,
-                timeout: 1000,
             },
         );
-        if (response.status === 200) {
-            const accessToken = response.headers.access_token;
-            console.log(accessToken);
-            axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
-
-            return true;
+        const accessToken = response.headers.access_token;
+        if (accessToken) {
+            return accessToken;
         }
-        return false;
+        throw new Error('reissue Error');
     } catch (error) {
         console.log(error, 'reissue Error');
-        throw Error;
+        throw error;
+    }
+};
+
+// 액세스 토큰 재발급 후 재요청
+const retryRequestWithNewToken = async (
+    originalRequest: AxiosRequestConfig,
+    newAccessToken: string,
+) => {
+    localStorage.setItem('accessToken', newAccessToken);
+
+    const modifiedConfig = { ...originalRequest };
+    modifiedConfig.headers = originalRequest.headers || {};
+    modifiedConfig.headers.Authorization = `Bearer ${newAccessToken}`;
+    try {
+        return await axios(originalRequest);
+    } catch (error) {
+        console.error('재요청 실패');
+        window.location.replace('/login');
+        return Promise.reject(error);
     }
 };
 
@@ -67,16 +84,18 @@ axiosAuth.interceptors.response.use(
         const errorStatus = error.response.status;
         const originalRequest = error.config;
 
-        // 토근 만료시 access token 재발급
         if (errorStatus === 401) {
             try {
-                const response = await refreshAccessToken();
-                if (response) {
-                    return await axios(originalRequest);
+                const newAccessToken = await refreshAccessToken();
+                if (newAccessToken) {
+                    return await retryRequestWithNewToken(
+                        originalRequest,
+                        newAccessToken,
+                    );
                 }
             } catch (err) {
                 alert('토큰이 만료되었습니다. 다시 로그인 해주세요');
-                // window.location.replace('/auth');
+                window.location.replace('/login');
             }
         }
         // 에러 반환.
